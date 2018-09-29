3b1e2f8827a3e8f86222ed19aa5e5a33bc41dadd
Paul Clark <clark.paul123@gmail.com>
2018-09-29 09:37:43 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,6 +9,21 @@ class Person
   end
   
   def name=(new_name)
-    
+    @name = new_name
+  end
+  
+  def initialise(job)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
   
 end 
+
+beyonce = Person.new_job
